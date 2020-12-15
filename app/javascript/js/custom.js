97ac86c2bccca08f5166f3a3292e09ef7a3119bc
magas4891 <magasishe@gmail.com>
2020-12-15 19:18:16 +0200

    to office

---
+++ app/javascript/js/custom.js
@@ -0,0 +1,17 @@
+$(document).on("click", ("form"), function(event){
+    event.preventDefault();
+
+
+    // var proj_id = $(this).first().data("edit");
+    // var proj_inp = $(`li#project-${proj_id} input#project_name`);
+    // if (proj_inp.attr("disabled", true)) {
+    //     console.log("-------");
+    //     proj_inp.attr("disabled", false);
+    //     return false;
+    // }
+    // if (!proj_inp.attr("disabled")) {
+    //     console.log("+++++++");
+    //     proj_inp.parents('form').submit();
+    //     proj_inp.attr("disabled", true);
+    // }
+});
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 
+require("../js/custom");
+
 import jquery from 'jquery';
 window.$ = window.jquery = jquery;
 
